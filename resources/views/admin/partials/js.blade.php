19a58c860f2fe6e65f76877aedc05fd88e7c15e0
abbibr <ibrohim_abbosov@mail.ru>
2022-09-10 15:22:43 +0500

    text

--- resources/views/admin/partials/js.blade.php
+++ resources/views/admin/partials/js.blade.php
@@ -16,4 +16,4 @@
 
 
  <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>Insane Like Me – Gasoline
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
