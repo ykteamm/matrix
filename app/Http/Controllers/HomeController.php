54a2497a83ee610de45ed80f63c306212321b8aa
abbibr <ibrohim_abbosov@mail.ru>
2022-09-28 12:38:55 +0500

    smsapi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -77,7 +77,7 @@ class HomeController extends Controller
         curl_close($ch);
 
         return [
-            'res' => $post
+            'res' => $responses['reason']
         ];
     }
     public function reg()
