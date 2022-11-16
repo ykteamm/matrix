85e2e15dd5508765e872e55553d8ef8696ea617b
EBirodar97 <92210441+EBirodar@users.noreply.github.com>
2022-11-16 11:18:20 +0500

    update user dorixona

--- app/Http/Controllers/PharmUsersController.php
+++ app/Http/Controllers/PharmUsersController.php
@@ -19,7 +19,7 @@ class PharmUsersController extends Controller
         foreach ($phar_user as $id){
             $arr[]=$id->user_id;
         }
-        $users=User::where('admin',TRUE)
+        $users=User::where('status',1)
             ->whereNotIn('id',$arr)
             ->get();
         $pharmacies=Pharmacy::all();
