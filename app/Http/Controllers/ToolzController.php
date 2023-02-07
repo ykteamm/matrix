7828a61a98504aae8b9224f2cf6f3f7868ddd895
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-02-07 14:29:53 +0500

    king-sold

--- app/Http/Controllers/ToolzController.php
+++ app/Http/Controllers/ToolzController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\KingSold;
 use App\Models\Shift;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 class ToolzController extends Controller
 {
     public function selfi()
@@ -29,6 +31,7 @@ class ToolzController extends Controller
     {  
         $solds = KingSold::with('order','order.sold','order.sold.medicine','order.user')
         ->whereDate('created_at','>=','2023-01-30')
+        ->where('image','!=','add')
         ->orderBy('id','DESC')->get();
         $host = substr(request()->getHttpHost(),0,3);
         return view('toolz.king-sold',compact('solds','host'));
@@ -36,6 +39,24 @@ class ToolzController extends Controller
     }
     public function kingSoldAnsver(Request $request)
     {
+        // return $request->id;
+        $order_id = KingSold::find($request->id)->order_id;
+        // return $order_id;
+        $summa = DB::table('tg_productssold')
+            ->selectRaw('SUM(tg_productssold.number * tg_productssold.price_product) as allprice')
+            ->where('tg_productssold.order_id',$order_id)->get();
+        $add = floor($summa[0]->allprice/200000)-1;
+        if($add != 0)
+        {
+            for ($i=1; $i <= $add; $i++) { 
+                $new = new KingSold([
+                    'order_id' => $order_id,
+                    'image' => 'add',
+                    'admin_check' => 1
+                ]);
+                $new->save();
+            }
+        }
         $new = KingSold::where('id',$request->id)->update([
             'admin_check' => $request->ansver
         ]);
