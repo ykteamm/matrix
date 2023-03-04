c753e2e1d8a83804d2fb2c0abfcdd96d894b0c4e
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-03-04 16:41:26 +0500

    gg

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -175,9 +175,9 @@
 </div>
 @endsection
 @section('admin_script')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js"></script>
-<script src="{{asset('/assets/js/moment.tz.min.js')}}"></script>
-
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js"></script>
+<script src="{{asset('/assets/js/moment.tz.min.js')}}"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 <script>
    $(function() {
   $('input[name="datetimes"]').daterangepicker({
@@ -275,23 +275,26 @@
 
                    $.each(response.data, function(index, value){
 
-                     var newYork    = moment.tz(value.m_data, "Europe/London");
+                     // var newYork    = moment.tz(value.m_data, "UTC");
+                     var newYork = moment(value.m_data).utc();
                      // var losAngeles = newYork.clone().tz("America/Los_Angeles");
                      // var london     = newYork.clone().tz("Europe/London");
-
+                     console.log(newYork);
                      // newYork.format();    // 2016-06-01T12:00:00-04:00
                      // losAngeles.format(); // 2016-06-01T09:00:00-07:00
                      // london.format();     // 2016-06-01T17:00:00+01:00
-                     var dateff = new Date(newYork.format());
+                     var dateff = new Date(newYork);
+                     var d = new Date(newYork);
+
                      // var fsdf = addHours(5, dateff);
-                     var fsdf = dateff;
+                     // var fsdf = dateff;
 
-                     var e = formatDate(fsdf);
+                     // var e = formatDate(fsdf);
 
-                      var d = new Date(value.m_data);
+                     //  var d = new Date(value.m_data);
 
                      // var newd = addHours(5, value.m_data)
-                     var dateff = new Date(value.m_data);
+                     // var dateff = new Date(value.m_data);
                      // var fsdf = addHours(5, dateff);
 
                             var curr_day = d.getDate();
