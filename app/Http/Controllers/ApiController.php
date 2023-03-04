2a9ca706eb0802be6e1ac05422d8095136eefdfe
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-03-04 15:10:58 +0500

    ff

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,7 +9,9 @@ class ApiController extends Controller
 {
     public function solds10()
     {
-        $solds = DB::table('tg_productssold')->limit('10')->orderby('id','DESC')->get();
-        return $solds;
+        $solds = DB::table('tg_productssold')->limit('10')->orderby('id','DESC')->first();
+        $day = date('Y-m-d',strtotime($solds->created_at));
+        $hour = date('H:i',strtotime($solds->created_at));
+        return $solds->created_at.' T '.$day.' T '.$hour;
     }
 }
