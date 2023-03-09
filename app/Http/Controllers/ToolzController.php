5818b8193df77a06ae5cf2288e6d6440be4d5d09
Abdumannon <abdphyr@gmail.com>
2023-03-09 18:11:59 +0500

    change kingSoldAnswer

--- app/Http/Controllers/ToolzController.php
+++ app/Http/Controllers/ToolzController.php
@@ -7,6 +7,7 @@ use App\Interfaces\Repositories\ShiftRepository;
 use App\Models\KingSold;
 use App\Models\LigaKingSold;
 use App\Models\LigaKingUser;
+use App\Models\Order;
 use App\Models\ProductSold;
 use App\Models\Shift;
 use App\Models\User;
@@ -131,64 +132,88 @@ class ToolzController extends Controller
         }
         return view('toolz.king-sold-history', compact('solds', 'host', 'date', 'paginated'));
     }
+    
     public function kingSoldAnsver(Request $request)
     {
         $order_id = KingSold::find($request->id)->order_id;
         $summa = DB::table('tg_productssold')
             ->selectRaw('SUM(tg_productssold.number * tg_productssold.price_product) as allprice')
             ->where('tg_productssold.order_id', $order_id)->get();
-
-
-        if ($summa[0]->allprice >= 200000) {
-            $add = floor($summa[0]->allprice / 200000) - 1;
-
-            if ($add != 0) {
-                for ($i = 1; $i <= $add; $i++) {
-                    $new = new KingSold([
-                        'order_id' => $order_id,
-                        'image' => 'add',
-                        'admin_check' => 1
-                    ]);
-                    $new->save();
-                }
+        if ($request->ansver == 2) {
+            if ($summa[0]->allprice >= 200000) {
+                $new = KingSold::where('id', $request->id)->update([
+                    'admin_check' => $request->ansver,
+                    'status' => 1
+                ]);
+            } else {
+                $new = KingSold::where('id', $request->id)->update([
+                    'admin_check' => $request->ansver,
+                    'status' => 2
+                ]);
             }
-            $new = KingSold::where('id', $request->id)->update([
-                'admin_check' => $request->ansver,
-                'status' => 1
-            ]);
         } else {
-            $new = KingSold::where('id', $request->id)->update([
-                'admin_check' => $request->ansver,
-                'status' => 2
+
+            if ($summa[0]->allprice >= 200000) {
+                $add = floor($summa[0]->allprice / 200000) - 1;
+
+                if ($add != 0) {
+                    for ($i = 1; $i <= $add; $i++) {
+                        $new = new KingSold([
+                            'order_id' => $order_id,
+                            'image' => 'add',
+                            'admin_check' => 1
+                        ]);
+                        $new->save();
+                    }
+                }
+                $new = KingSold::where('id', $request->id)->update([
+                    'admin_check' => $request->ansver,
+                    'status' => 1
+                ]);
+            } else {
+                $new = KingSold::where('id', $request->id)->update([
+                    'admin_check' => $request->ansver,
+                    'status' => 2
+                ]);
+            }
+
+            $user_id = Order::find($order_id)->user_id;
+
+            $exists_user_id = DB::table('liga_king_users')->where('user_id', $user_id)->get();
+            if (count($exists_user_id) == 0) {
+                DB::table('liga_king_users')->insert([
+                    'liga_id' => 2,
+                    'user_id' => $user_id
+                ]);
+            }
+
+            $weekStartDate = Carbon::now()->startOfWeek()->format('Y-m-d');
+            $weekEndDate = Carbon::now()->endOfWeek()->format('Y-m-d');
+            $king_sold = DB::table('tg_king_sold')
+                ->selectRaw('tg_order.user_id,tg_user.phone_number')
+                ->where('tg_king_sold.admin_check', 1)
+                ->whereDate('tg_king_sold.created_at', '>=', $weekStartDate)
+                ->whereDate('tg_king_sold.created_at', '<=', $weekEndDate)
+                ->join('tg_order', 'tg_order.id', 'tg_king_sold.order_id')
+                ->join('tg_user', 'tg_user.id', 'tg_order.user_id')
+                ->groupBy('tg_order.user_id', 'tg_user.phone_number')
+                ->pluck('phone_number')->toArray();
+            $user_id = DB::table('tg_order')->where('id', $order_id)->value('user_id');
+            $user = DB::table('tg_user')->find($user_id);
+            $king_sold[] = '+998990821015';
+            $king_sold[] = '+998977332305';
+            $king_sold[] = '+998931810408';
+            $king_sold[] = '+998930000047';
+            $king_sold[] = '+998935050498';
+            $king_sold[] = '+998946890010';
+
+            $king_sold[] = '+998990316244';
+            $king_sold[] = '+998995511944';
+            $response = Http::post('notify.eskiz.uz/api/auth/login', [
+                'email' => 'mubashirov2002@gmail.com',
+                'password' => 'PM4g0AWXQxRg0cQ2h4Rmn7Ysoi7IuzyMyJ76GuJa'
             ]);
-        }
-
-
-        $weekStartDate = Carbon::now()->startOfWeek()->format('Y-m-d');
-        $weekEndDate = Carbon::now()->endOfWeek()->format('Y-m-d');
-        $king_sold = DB::table('tg_king_sold')
-            ->selectRaw('tg_order.user_id,tg_user.phone_number')
-            ->where('tg_king_sold.admin_check', 1)
-            ->whereDate('tg_king_sold.created_at', '>=', $weekStartDate)
-            ->whereDate('tg_king_sold.created_at', '<=', $weekEndDate)
-            ->join('tg_order', 'tg_order.id', 'tg_king_sold.order_id')
-            ->join('tg_user', 'tg_user.id', 'tg_order.user_id')
-            ->groupBy('tg_order.user_id', 'tg_user.phone_number')
-            ->pluck('phone_number')->toArray();
-        $user_id = DB::table('tg_order')->where('id', $order_id)->value('user_id');
-        $user = DB::table('tg_user')->find($user_id);
-        $king_sold[] = '+998990821015';
-        $king_sold[] = '+998977332305';
-        $king_sold[] = '+998931810408';
-        $king_sold[] = '+998930000047';
-        $king_sold[] = '+998935050498';
-        $king_sold[] = '+998946890010';
-        $response = Http::post('notify.eskiz.uz/api/auth/login', [
-            'email' => 'mubashirov2002@gmail.com',
-            'password' => 'PM4g0AWXQxRg0cQ2h4Rmn7Ysoi7IuzyMyJ76GuJa'
-        ]);
-        $token = $response['data']['token'];
-        if ($request->ansver != 2) {
+            $token = $response['data']['token'];
             foreach ($king_sold as $key => $value) {
                 $sms = Http::withToken($token)->post('notify.eskiz.uz/api/message/sms/send', [
                     'mobile_phone' => substr($value, 1),
@@ -203,6 +228,79 @@ class ToolzController extends Controller
             'response' => 200,
         ];
     }
+
+    // public function kingSoldAnsver(Request $request)
+    // {
+    //     $order_id = KingSold::find($request->id)->order_id;
+    //     $summa = DB::table('tg_productssold')
+    //         ->selectRaw('SUM(tg_productssold.number * tg_productssold.price_product) as allprice')
+    //         ->where('tg_productssold.order_id', $order_id)->get();
+
+
+    //     if ($summa[0]->allprice >= 200000) {
+    //         $add = floor($summa[0]->allprice / 200000) - 1;
+
+    //         if ($add != 0) {
+    //             for ($i = 1; $i <= $add; $i++) {
+    //                 $new = new KingSold([
+    //                     'order_id' => $order_id,
+    //                     'image' => 'add',
+    //                     'admin_check' => 1
+    //                 ]);
+    //                 $new->save();
+    //             }
+    //         }
+    //         $new = KingSold::where('id', $request->id)->update([
+    //             'admin_check' => $request->ansver,
+    //             'status' => 1
+    //         ]);
+    //     } else {
+    //         $new = KingSold::where('id', $request->id)->update([
+    //             'admin_check' => $request->ansver,
+    //             'status' => 2
+    //         ]);
+    //     }
+
+
+    //     $weekStartDate = Carbon::now()->startOfWeek()->format('Y-m-d');
+    //     $weekEndDate = Carbon::now()->endOfWeek()->format('Y-m-d');
+    //     $king_sold = DB::table('tg_king_sold')
+    //         ->selectRaw('tg_order.user_id,tg_user.phone_number')
+    //         ->where('tg_king_sold.admin_check', 1)
+    //         ->whereDate('tg_king_sold.created_at', '>=', $weekStartDate)
+    //         ->whereDate('tg_king_sold.created_at', '<=', $weekEndDate)
+    //         ->join('tg_order', 'tg_order.id', 'tg_king_sold.order_id')
+    //         ->join('tg_user', 'tg_user.id', 'tg_order.user_id')
+    //         ->groupBy('tg_order.user_id', 'tg_user.phone_number')
+    //         ->pluck('phone_number')->toArray();
+    //     $user_id = DB::table('tg_order')->where('id', $order_id)->value('user_id');
+    //     $user = DB::table('tg_user')->find($user_id);
+    //     $king_sold[] = '+998990821015';
+    //     $king_sold[] = '+998977332305';
+    //     $king_sold[] = '+998931810408';
+    //     $king_sold[] = '+998930000047';
+    //     $king_sold[] = '+998935050498';
+    //     $king_sold[] = '+998946890010';
+    //     $response = Http::post('notify.eskiz.uz/api/auth/login', [
+    //         'email' => 'mubashirov2002@gmail.com',
+    //         'password' => 'PM4g0AWXQxRg0cQ2h4Rmn7Ysoi7IuzyMyJ76GuJa'
+    //     ]);
+    //     $token = $response['data']['token'];
+    //     if ($request->ansver != 2) {
+    //         foreach ($king_sold as $key => $value) {
+    //             $sms = Http::withToken($token)->post('notify.eskiz.uz/api/message/sms/send', [
+    //                 'mobile_phone' => substr($value, 1),
+    //                 'message' => 'Jangchi !!! ' . ' ' . $user->last_name . ' ' . $user->first_name . ' ' . 'hozirgina shox yurish qildi.' . ' ' . 'Yutganga 200.000 som premiya beriladi!!! https://jang.novatio.uz',
+    //                 'from' => '4546',
+    //                 'callback_url' => 'http://0000.uz/test.php'
+    //             ]);
+    //         }
+    //     }
+
+    //     return [
+    //         'response' => 200,
+    //     ];
+    // }
     public function kingSoldLiga()
     {
         $ligas = LigaKingSold::with('liga_user', 'liga_user.user')->get();
