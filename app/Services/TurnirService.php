508ce58711ec97f231c9adbbfb500984d6cd8961
abdumannon <abdphyr@gmail.com>
2023-06-17 00:16:17 +0500

    Turnir play off service

--- app/Services/TurnirService.php
+++ app/Services/TurnirService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\TurnirMember;
+use App\Models\TurnirPlayoff;
 use App\Models\TurnirPoint;
 use App\Models\TurnirStanding;
 use App\Models\TurnirTeam;
@@ -60,13 +61,89 @@ class TurnirService
         $team2Ids = $this->getTeamIds($month, 'team2_id', $s, $tour)->toArray();
         return array_merge($team1Ids, $team2Ids);
     }
-    
+
 
     public function storeMembersPointsBattles($request, $status)
     {
         $tour = $this->getNextTour();
         $month = $this->month;
-        // $team1Uids = $this->getTeamUids($request->team1_id, $tour->tour-1, $month);
+
+        try {
+            $battle = TurnirStanding::create([
+                'team1_id' => $request->team1_id,
+                'team2_id' => $request->team2_id,
+                'win' => null,
+                'lose' => null,
+                'tour' => $tour->tour,
+                'date_begin' => $tour->date_begin,
+                'date_end' => $tour->date_end,
+                'status' => $status,
+                'month' => $month,
+                'ends' => 0,
+            ]);
+            if ($status == 1) {
+                $node = DB::table('turnir_playoffs')
+                    ->where('battle_id', NULL)
+                    ->whereDate('month', $month)
+                    ->orderBy('node', 'ASC')
+                    ->first();
+                TurnirPlayoff::where('id', $node->id)->update([
+                    'battle_id' => $battle->id
+                ]);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function getTeamsWithoutBattle($status)
+    {
+        $month = $this->month;
+        $tour = $this->getNextTour();
+        $teamIds = $this->getTeamsIds($month, $status, $tour->tour);
+        // dd($teamIds);
+        return TurnirTeam::with(['turnir_member' => function ($q) use ($month) {
+            $q->whereDate('month', $month);
+            $q->orderBy('tour');
+        }, 'turnir_member.user' => function ($u) {
+            $u->select('id', 'first_name', 'last_name');
+        }])
+            ->where('status', $status)
+            ->whereNotIn('id', $teamIds)
+            ->get();
+    }
+
+    public function getStandings($status)
+    {
+        $month = $this->month;
+        $tour = $this->getNextTour();
+        return TurnirStanding::select('id', 'team1_id', 'team2_id')
+            ->with([
+                'team1' => function ($q) use ($month, $status) {
+                    $q->select('id')->where('status', $status);
+                    $q->with(['users' => function ($b) use ($month) {
+                        $b->whereDate('month', $month)->groupBy('tg_user.id', 'turnir_members.team_id')
+                            ->selectRaw('tg_user.id, tg_user.first_name as f, tg_user.last_name as l, tg_user.image_url as img, turnir_members.team_id')
+                            ->leftJoin('tg_user', 'tg_user.id', 'turnir_members.user_id');
+                    }]);
+                },
+                'team2' => function ($q) use ($month, $status) {
+                    $q->select('id', 'status')->where('status', $status);
+                    $q->with(['users' => function ($b) use ($month) {
+                        $b->whereDate('month', $month)->groupBy('tg_user.id', 'turnir_members.team_id')
+                            ->selectRaw('tg_user.id, tg_user.first_name as f, tg_user.last_name as l, tg_user.image_url as img, turnir_members.team_id')
+                            ->leftJoin('tg_user', 'tg_user.id', 'turnir_members.user_id');
+                    }]);
+                }
+            ])
+            ->where('status', $status)
+            ->where('tour', $tour->tour)
+            ->whereDate('month', $month)
+            ->get();
+    }
+}
+
+// $team1Uids = $this->getTeamUids($request->team1_id, $tour->tour-1, $month);
         // $team2Uids = $this->getTeamUids($request->team2_id, $tour->tour-1, $month);
         // dd($request->all(), $status);
         // TurnirMember::create([
@@ -105,63 +182,3 @@ class TurnirService
         //     'tour' => $tour->tour,
         //     'month' => $month
         // ]);
-        TurnirStanding::create([
-            'team1_id' => $request->team1_id,
-            'team2_id' => $request->team2_id,
-            'win' => null,
-            'lose' => null,
-            'tour' => $tour->tour,
-            'date_begin' => $tour->date_begin,
-            'date_end' => $tour->date_end,
-            'status' => $status,
-            'month' => $month,
-            'ends' => 0,
-        ]); 
-    }
-
-    public function getTeamsWithoutBattle($status)
-    {
-        $month = $this->month;
-        $tour = $this->getNextTour();
-        $teamIds = $this->getTeamsIds($month, $status, $tour->tour);
-        // dd($teamIds);
-        return TurnirTeam::with(['turnir_member' => function ($q) use ($month) {
-            $q->whereDate('month', $month);
-            $q->orderBy('tour');
-        }, 'turnir_member.user' => function ($u) {
-            $u->select('id', 'first_name', 'last_name');
-        }])
-            ->where('status', $status)
-            ->whereNotIn('id', $teamIds)
-            ->get();
-    }
-
-    public function getStandings($status)
-    {
-        $month = $this->month;
-        $tour = $this->getNextTour();
-        return TurnirStanding::select('id', 'team1_id', 'team2_id')
-        ->with([
-            'team1' => function ($q) use ($month, $status) {
-                $q->select('id')->where('status', $status);
-                $q->with(['users' => function ($b) use ($month) {
-                    $b->whereDate('month', $month)->groupBy('tg_user.id', 'turnir_members.team_id')
-                        ->selectRaw('tg_user.id, tg_user.first_name as f, tg_user.last_name as l, tg_user.image_url as img, turnir_members.team_id')
-                        ->leftJoin('tg_user', 'tg_user.id', 'turnir_members.user_id');
-                }]);
-            },
-            'team2' => function ($q) use ($month, $status) {
-                $q->select('id', 'status')->where('status', $status);
-                $q->with(['users' => function ($b) use ($month) {
-                    $b->whereDate('month', $month)->groupBy('tg_user.id', 'turnir_members.team_id')
-                        ->selectRaw('tg_user.id, tg_user.first_name as f, tg_user.last_name as l, tg_user.image_url as img, turnir_members.team_id')
-                        ->leftJoin('tg_user', 'tg_user.id', 'turnir_members.user_id');
-                }]);
-            }
-        ])
-        ->where('status', $status)
-        ->where('tour', $tour->tour)
-        ->whereDate('month', $month)
-        ->get();        
-    }
-}
