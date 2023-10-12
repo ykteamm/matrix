3392eebd386d8c474956740eb42547a07f97631b
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-10-12 11:08:38 +0500

    fd

--- app/Http/Controllers/RekrutController.php
+++ app/Http/Controllers/RekrutController.php
@@ -191,7 +191,7 @@ class RekrutController extends Controller
         $ddd = date_format($date,"Y-m-d");
 
         $rekrut = Rekrut::with('region')->where('region_id',$id)->where('group_id',2)
-        ->whereIn('xolat',[1,2,3])
+        ->whereIn('xolat',[1,2,3,4])
         ->whereDate('created_at','>=','2023-10-10')
         ->whereDate('created_at','<=','2023-10-11')
         ->get();
