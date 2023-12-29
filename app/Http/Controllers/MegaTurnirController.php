7116c41d371256e11f28b387d8ed4aab46d67d44
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-12-29 10:19:43 +0500

    gf

--- app/Http/Controllers/MegaTurnirController.php
+++ app/Http/Controllers/MegaTurnirController.php
@@ -157,8 +157,8 @@ class MegaTurnirController extends Controller
         $teach = new MegaTurnirUserBattle;
         $teach->user1id = $request->user1id;
         $teach->user2id = $request->user2id;
-        $teach->begin = '2023-12-29';
-        $teach->end = '2023-12-30';
+        $teach->begin = '2023-12-28';
+        $teach->end = '2023-12-28';
         $teach->tour = 200;
         $teach->save();
 
